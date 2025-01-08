4ae3fb7ceaed8140de2291e41f793cc7570c196b
杜宇航 <duyuhang@huanqiu.com>
2025-01-08 16:08:10 +0800

    feat:add comfyui static

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .nodes.StaticResource import StaticResource
+
+
+NODE_CLASS_MAPPINGS = {
+    "StaticResource":StaticResource
+}
+NODE_DISPLAY_NAMES_MAPPINGS = {
+}
+WEB_DIRECTORY = "./js"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAMES_MAPPINGS','WEB_DIRECTORY']
---
+++ nodes/StaticResource.py
@@ -0,0 +1,41 @@
+import folder_paths
+import requests
+import os
+
+class StaticResource():
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": { 
+                "resource_url" : ("STRING", {"default": ""}),
+                "filename_prefix": ("STRING", {"default": "comfyui-static-resource"}),
+            },
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "execute"
+    OUTPUT_NODE = True
+    CATEGORY = "static-resource"
+
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+
+
+    def execute(self, resource_url, filename_prefix):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, 0, 0)
+        file = f"{filename}_{counter:05}_.zip"
+        local_file_path = os.path.join(full_output_folder, file)
+        download_file(resource_url, local_file_path)
+        return {}
+    
+    
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
