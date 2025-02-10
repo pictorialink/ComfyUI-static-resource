9a817d82979690b01227a6b5563a028729931ad7
杜宇航 <duyuhang@huanqiu.com>
2025-02-10 17:32:08 +0800

    fix:优化

--- nodes/StaticResource.py
+++ nodes/StaticResource.py
@@ -1,3 +1,4 @@
+import json
 import folder_paths
 import requests
 import os
@@ -13,7 +14,7 @@ class StaticResource():
         }
 
     RETURN_TYPES = ()
-    FUNCTION = "execute"
+    FUNCTION = "execute2"
     OUTPUT_NODE = True
     CATEGORY = "static-resource"
 
@@ -38,7 +39,33 @@ class StaticResource():
         })
         return { "ui": { "images": results } }
     
+    def execute2(self, resource_url, filename_prefix):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, 0, 0)
+        file = f"{filename}_{counter:05}_.json"
+        local_file_path = os.path.join(full_output_folder, file)
+        data = {
+            "doodle_material": "https://github.com/solution9th/pictorial-static-doodle/archive/refs/heads/main.zip",
+            "poses": "https://github.com/solution9th/pictorial-static-poses/archive/refs/heads/main.zip",
+            "layer_material": "https://github.com/solution9th/pictorial-static-layer/archive/refs/heads/main.zip",
+            "style": "https://github.com/solution9th/pictorial-static-style/archive/refs/heads/main.zip",
+            "fonts": "https://github.com/solution9th/pictorial-static-fonts/archive/refs/heads/main.zip",
+            "faces": "https://github.com/solution9th/pictorial-static-faces/archive/refs/heads/main.zip"
+        } 
+        write_to_file(data, local_file_path)
+        results = list()
+        results.append({
+            "filename": file,
+            "subfolder": subfolder,
+            "type": self.type
+        })
+        return { "ui": { "images": results } }
     
+    
+def write_to_file(content, local_filename):
+    with open(local_filename, "w", encoding="utf-8") as file:
+        json.dump(content, file)  # indent 参数用于美化输出，缩进 4 个空格
+
 def download_file(url, local_filename):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
